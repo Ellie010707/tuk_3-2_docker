e36fda5a126b52ea73907f6ce90cc431ed809a0d
Ellie <doris0707@naver.com>
2022-11-06 23:32:42 +0900

    [feat] modify shell file

--- kyl1/kyl1_app/views.py
+++ kyl1/kyl1_app/views.py
@@ -13,11 +13,11 @@ def start(request):
 @api_view(['POST'])
 def game(request):
     num = request.data.get("num")
-    msg = ['kyl1', 'get', str(num) , '>']
+    msg = ['get', str(num) , '>']
     for s in str(num):
         if s == '3' or s == '6' or s == '9':
             msg.append("JJAK")
-    print(" ".join(msg))
-    r = requests.post('http://kyl2:8082/api/game',json={'num': num+1}, verify=False)
     time.sleep(2)
+    print(" ".join(msg))
+    requests.post('http://kyl2:8082/api/game',json={'num': num+1}, verify=False)
     return HttpResponse(" ")
