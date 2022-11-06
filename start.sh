1cf968c202aa6ef44d2298a93b0395ec216d5e85
Ellie <doris0707@naver.com>
2022-11-06 23:27:36 +0900

    [feat] modify shell file

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-mkdir kyl
-cd kyl
-git pull "https://github.com/Ellie010707/tuk_3-2_docker"
-sudo apt-get install virtualenv
-sudo apt-get install pip
-
-virtualenv -p python3 venv
-source venv/bin/activate
-
-sudo apt-get install httpie 
-http -v GET http://localhost:8081/api/start
+docker-compose up -d --build
+curl http://localhost:8081/api/start &
+docker-compose logs -f
